909459625b564568ec2d2b8c02ad1d9b9d007cfb
nguyenhoangtuananh2002 <165981275+nguyenhoangtuananh2002@users.noreply.github.com>
2025-04-25 08:28:35 +0700

    ADD: CORS configuration

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 from config import AppConfig
 
 
@@ -10,6 +11,7 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(AppConfig) 
+    CORS(app)
 
     
     db.init_app(app)
