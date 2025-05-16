6d028e4f0a35fb14041ef15dbcf3007960550d29
Ho Tuan Huy <huy.hotuan@vn.bosch.com>
2025-05-16 10:14:14 +0700

    Added API endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from config import AppConfig
+from auth.routes import auth_bp
+from chats.routes import chats_bp
+from location.routes import location_bp
 
+db = SQLAlchemy()  
+migrate = Migrate()  
 
 def create_app():
  
     app = Flask(__name__)
     print("CORS config for app init")
     CORS(app)
+    app.register_blueprint(chats_bp)
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(location_bp)
 
      # --- Place print here, after all registrations ---
     print("Registered Routes:")
